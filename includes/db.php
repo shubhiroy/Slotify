2b9101cc67534635a840f4b85d58718f2500cc1c
shubhiroy <shubham09roy@gmail.com>
2019-03-01 11:40:31 +0530

    Added validator funcitons defination

--- includes/db.php
+++ includes/db.php
@@ -36,18 +36,45 @@ class Account{
         print_r($this->error_arr); 
     }
     private function validateUsername($un){
+        if(strlen($un) > 25  || 5 > strlen($un)){
+            array_push($this->error_arr,"Username should be between 5 and 25.");
+            return;
+        }
+        //TODO: Check if the username already exists
     }
     private function validateFirstName($fn){
-
+        if(strlen($fn) > 30  || 2 > strlen($fn)){
+            array_push($this->error_arr,"First name should be between 2 and 30.");
+            return;
+        }
     }
     private function validateLastName($ln){
-
+        if(strlen($ln) > 30  || 2 > strlen($ln)){
+            array_push($this->error_arr,"Last name should be between 2 and 30.");
+            return;
+        }
     }
     private function validateEmails($em, $em2){
-
+        if($em != $em2){
+            array_push($this->error_arr,"Your emails don't match.");
+            return;
+        }
+        if(!filter_var($em,FILTER_VALIDATE_EMAIL)){
+            array_push($this->error_arr,"Email is invalid.");
+            return;
+        }
+        //TODO: Check if the email already exists
     }
     private function validatePasswords($pw, $pw2){
-
+        echo 'password field';
+        if($pw != $pw2){
+            array_push($this->error_arr,"Your passwords don't match.");
+            return;
+        }
+        if(preg_match('/[^A-Za-z0-9]/',$pw)){
+            array_push($this->error_arr,"Your passwords can only contain numbers and characters");
+            return;
+        }
     }
 }
 ?>
--- includes/register_handler.php
+++ includes/register_handler.php
@@ -8,7 +8,7 @@
         $email2 = $sanitizer->emailSanitizer($_POST['email2']);
         $password = $sanitizer->passwordSanitizer($_POST['password']);
         $password2 = $sanitizer->passwordSanitizer($_POST['password2']);
-
+        
         $account->register($username,$firstname,$lastname,$email,$email2,$password,$password2);
     }
 ?>
