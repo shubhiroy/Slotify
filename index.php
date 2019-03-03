1aea3d6edd748f6253a001d615f7fc4ea36854ef
shubhiroy <shubham09roy@gmail.com>
2019-03-04 01:38:37 +0530

    Created songs,artist,genres and albums table in db

--- index.php
+++ index.php
@@ -26,6 +26,24 @@
                         <img src="includes/assets/images/icons/logo.png" alt="Home">
                     </a>
                 </nav>
+
+                <div class="group">
+                    <div class="navItem">
+                        <a href="search.php" class="navItemLink">SEARCH</a>
+                        <img src="includes/assets/images/icons/search.png" alt="Search" class="searchIcon">
+                    </div>
+                </div>
+                <div class="group">
+                    <div class="navItem">
+                        <a href="search.php" class="navItemLink">Browse</a>
+                    </div>
+                    <div class="navItem">
+                        <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                    </div>
+                    <div class="navItem">
+                        <a href="profile.php" class="navItemLink">Rick Kennedy</a>
+                    </div>
+                </div>
             </div>
         </div>
     
