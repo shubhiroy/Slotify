d646c3ec9d06656f4a25266f56579b8bb967a102
shubhiroy <shubham09roy@gmail.com>
2019-03-09 14:41:26 +0530

    Added current time,remaining time and progress bar update feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,14 @@ class Audio {
         this.audio.addEventListener("canplay",function(){
             let duration = Audio.formatTime(this.duration);
             $(".progressTime.remaining").text(duration);
+            // let progressWidth = ((this.audio.currentTime/this.duration) * 100) + "%";
+            // console.log(progressWidth);
+            // $("#nowPlayingCenter.progress").css("width",progressWidth);
+        });
+        this.audio.addEventListener("timeupdate",function(){
+            if(this.duration){
+                Audio.updateProgressTime(this);
+            }
         });
     }
 
@@ -29,6 +37,16 @@ class Audio {
         let sec = time - (minutes * 60);
         let extraZero = (sec < 10) ? "0" : "";
         return minutes + ":" + extraZero + sec;  
+    }
+
+    static updateProgressTime(audio){
+        $(".progressTime.current").text(Audio.formatTime(audio.currentTime));
+        $(".progressTime.remaining").text(Audio.formatTime(audio.duration - audio.currentTime));
+        let progressWidth = ((audio.currentTime/audio.duration) * 100) + "%";
+        $(document).ready(function(){
+            $(".progress").css("width",progressWidth);
+        });
+        
     }
 
     setTrack(track){
