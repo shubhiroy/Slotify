c9b58692bd9414342b7be48d54e1311bf2732834
shubhiroy <shubham09roy@gmail.com>
2019-03-10 17:05:29 +0530

    Added url change on dynamic loading of pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,8 @@ function openURL(url){
     var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
     console.log(encodedUrl);
     $("#mainContent").load(encodedUrl);
+    $('body').scrollTop(0);
+    history.pushState(null,null,url);
 }
 
 class Audio {
