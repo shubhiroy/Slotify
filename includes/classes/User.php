89bae02b93987bf9f95938784580843e274c248d
shubhiroy <shubham09roy@gmail.com>
2019-03-14 03:01:05 +0530

    Added profile.php page and button stylings...

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -12,5 +12,13 @@
 		public function getUsername(){
 			return $this->userLoggedIn;
 		}
+
+		public function getFullName(){
+			$query = "Select concat(firstName,' ',lastName) as name from users where username = '$this->userLoggedIn'";
+			$result = mysqli_query($this->con,$query);
+			$resultArr = mysqli_fetch_array($result);
+			return $resultArr['name'];
+			// $resultArr['name']
+		}
 	}
 ?>
---
+++ profile.php
@@ -0,0 +1,9 @@
+<?php include_once("includes/includedFiles.php") ?>
+
+<div class="entityInfo" id="profile profilepage" style="display:block">
+    <div class="centerSection">
+    <div class="userInfo" ><?php  echo $user->getFullName(); ?></div>
+    </div>
+    <div class="button">USER DETAILS</div>
+    <div class="button green">LOG OUT</div>
+</div> 
